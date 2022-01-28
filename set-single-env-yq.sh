8ae01ecf43ce5534806d08daa9b0a690fb5e4269
SISILIA <soowh91@gmail.com>
2022-01-28 13:40:51 +0900

    single cluster rule update

--- set-single-env-yq.sh
+++ set-single-env-yq.sh
@@ -1,10 +1,10 @@
-if [[ ! $# -eq 4 ]]; then
-    echo "ERORR: set-single-env-yq.sh needs 4 arguments, not ""$#"
-    echo "USAGE : bash set-single-env-yq.sh [REPO_URL] [BRANCH_NAME] [CLUSTER_INFO_TYPE] [CLUSTER_INFO]"
-    echo "CLUSTER_INFO_TYPE : name / server"
+if [[ ! $# -eq 3 ]]; then
+    echo "ERORR: set-single-env-yq.sh needs 3 arguments, not ""$#"
+    echo "USAGE : bash set-single-env-yq.sh [REPO_URL] [BRANCH_NAME] [CLUSTER_NAME]"
     exit -1
 fi
 
+yq e --inplace '.metadata.name = '"$3"'-applications' application/app_of_apps/single-applications.yaml
 yq e --inplace '.spec.source.repoURL = "'"$1"'"' application/app_of_apps/single-applications.yaml
 yq e --inplace '.spec.source.targetRevision = "'"$2"'"' application/app_of_apps/single-applications.yaml
-yq e --inplace '.spec.destination.'"$3"' = "'"$4"'"' application/app_of_apps/single-applications.yaml
+yq e --inplace '.spec.destination.name = "'"$3"'"' application/app_of_apps/single-applications.yaml
