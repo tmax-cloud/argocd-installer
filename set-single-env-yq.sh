0fa2af730cb98501fd4d3700e730371fb6e3c0fc
SISILIA <soowh91@gmail.com>
2022-03-22 19:58:32 +0900

    [grafana] single cluster client config 추가

--- set-single-env-yq.sh
+++ set-single-env-yq.sh
@@ -1,6 +1,6 @@
-if [[ ! $# -eq 4 ]]; then
-    echo "ERORR: set-single-env-yq.sh needs 4 arguments, not ""$#"
-    echo "USAGE : bash set-single-env-yq.sh [REPO_URL] [BRANCH_NAME] [CLUSTER_NAME] [PRIVATE_REGISTRY_URL]"
+if [[ ! $# -eq 5 ]]; then
+    echo "ERORR: set-single-env-yq.sh needs 5 arguments, not ""$#"
+    echo "USAGE : bash set-single-env-yq.sh [REPO_URL] [BRANCH_NAME] [CLUSTER_NAME] [PRIVATE_REGISTRY_URL] [SINGLE_CLUSTER_ADMIN_EMAIL]"
     exit -1
 fi
 
@@ -16,3 +16,4 @@ yq e --inplace '.spec.source.targetRevision = "'"$2"'"' "$newFile"
 yq e --inplace '.spec.source.helm.parameters[0].value = "'"$3"'"' "$newFile"
 yq e --inplace '.spec.source.helm.parameters[1].value = "'"$3"'"' "$newFile"
 yq e --inplace '.spec.source.helm.parameters[2].value = "'"$4"'"' "$newFile"
+yq e --inplace '.spec.source.helm.parameters[3].value = "'"$5"'"' "$newFile"
